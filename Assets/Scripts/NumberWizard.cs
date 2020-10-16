dffad8b2978c14857c5018e356483e17b605cef1
Emily Parker <emdebted123@gmail.com>
2020-10-16 00:47:36 -0500

    Imported and refactored code

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    void Start()
+    {
+        StartGame();
+    }
+    void StartGame()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        max = max + 1;
+    }
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
